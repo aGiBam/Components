24273cf5c4b9cfabd91ac0621020cdf07d56a108
MohamedAgiba <mohamed.agiba@cic.ae>
2026-08-24 12:38:44 +0300

    feat(app): replace default app template with core components
    add imports for ElementsHome and CollectionsHome components, update root app template to include the two feature home components alongside router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ElementsHome } from "./elements/elements-home/elements-home";
+import { CollectionsHome } from "./collections/collections-home/collections-home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ElementsHome, CollectionsHome],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
