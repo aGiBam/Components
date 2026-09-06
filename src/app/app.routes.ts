0cef85695bb46a2b4e4330bbed5077a644cb22a5
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-06 10:29:38 +0300

    feat(app): add Home and NotFound components with routing support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ElementsHome } from './elements/elements-home/elements-home';
 import { CollectionsHome } from './collections/collections-home/collections-home';
+import { Home } from './home/home';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'elements', component: ElementsHome },
   { path: 'collections', component: CollectionsHome },
+  { path: '**', component: NotFound},
 ];
