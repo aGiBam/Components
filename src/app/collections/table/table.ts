124319000e6f7f79574c45cf20498ea26fbb9c2a
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-08 13:58:23 +0300

    feat(collections): enhance Table component with dynamic class support and update divider text

--- src/app/collections/table/table.ts
+++ src/app/collections/table/table.ts
@@ -1,12 +1,14 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgClass } from '@angular/common';
 import { Component, Input} from '@angular/core';
 @Component({
-  imports: [JsonPipe],
+  imports: [JsonPipe, NgClass],
   selector: 'app-table',
   styleUrl: './table.css',
   templateUrl: './table.html',
 })
 export class Table {
+  @Input('class') classNames = '';
+
   @Input() data: any[] = [];
   @Input() headers: any[] = [];
 }
