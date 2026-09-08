27c40654525b75fc1ec628b1cd30348659158176
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-08 12:20:44 +0300

    feat(elements): update Segment component to use ng-content for dynamic content

--- src/app/elements/segment/segment.ts
+++ src/app/elements/segment/segment.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
 
 @Component({
-  imports: [],
+  imports: [ɵEmptyOutletComponent],
   selector: 'app-segment',
   styleUrl: './segment.css',
   templateUrl: './segment.html',
