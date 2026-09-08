5f912369b000a0c400a04dd0fecd5c9fb9ed454f
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-08 11:38:05 +0300

    feat(elements): add Divider component and integrate it into ElementsHome

--- src/app/elements/elements-home/elements-home.ts
+++ src/app/elements/elements-home/elements-home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Placeholder } from '../placeholder/placeholder';
+import { Divider } from '../../shared/divider/divider';
 @Component({
-  imports: [Placeholder],
+  imports: [Placeholder, Divider],
   selector: 'app-elements-home',
   styleUrl: './elements-home.css',
   templateUrl: './elements-home.html',
---
+++ src/app/shared/divider/divider.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-divider',
+  styleUrl: './divider.css',
+  templateUrl: './divider.html',
+})
+export class Divider {}
