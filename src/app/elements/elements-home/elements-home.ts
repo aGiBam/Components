db94f08838b42d8a453d039a6db4f6431940c3e4
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-08 11:58:49 +0300

    feat(elements): integrate Segment component into ElementsHome and update its template

--- src/app/elements/elements-home/elements-home.ts
+++ src/app/elements/elements-home/elements-home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Placeholder } from '../placeholder/placeholder';
 import { Divider } from '../../shared/divider/divider';
+import { Segment } from "../segment/segment";
 @Component({
-  imports: [Placeholder, Divider],
+  imports: [Placeholder, Divider, Segment],
   selector: 'app-elements-home',
   styleUrl: './elements-home.css',
   templateUrl: './elements-home.html',
