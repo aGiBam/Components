4b680a28a77745f0d4e1c7ef691a5b97b4f624ac
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-08 12:42:20 +0300

    feat(collections): add Table component with template, styles, and tests

--- src/app/collections/collections-home/collections-home.ts
+++ src/app/collections/collections-home/collections-home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Table } from '../table/table';
+import { Divider } from '../../shared/divider/divider';
 @Component({
-  imports: [],
+  imports: [Table, Divider],
   selector: 'app-collections-home',
   styleUrl: './collections-home.css',
   templateUrl: './collections-home.html',
