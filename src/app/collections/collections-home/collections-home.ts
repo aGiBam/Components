0214e6b5fd1df434ca6b932f0061310b45fdfef3
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-10 02:09:05 +0300

    feat(collections): add Tab Menu with routing support in CollectionsHome component

--- src/app/collections/collections-home/collections-home.ts
+++ src/app/collections/collections-home/collections-home.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { Table } from '../table/table';
 import { Divider } from '../../shared/divider/divider';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterOutlet, RouterLink } from '@angular/router';
 @Component({
-  imports: [Table, Divider, RouterOutlet],
+  imports: [Table, Divider, RouterOutlet, RouterLinkActive, RouterLink ],
   selector: 'app-collections-home',
   styleUrl: './collections-home.css',
   templateUrl: './collections-home.html',
 })
 export class CollectionsHome {
   data = [
-    { name: 'H.Kakeh', age: 27, job: 'FE Team Lead', employed: true },
+    { name: 'H.Kakeh', age: 27, job: 'Senior FE Team Lead', employed: true },
     { name: 'M.Agiba', age: 26, job: 'Mid-Level FE ', employed: false },
     { name: 'Shouq', age: 25, job: 'Junior FE', employed: true },
   ];
