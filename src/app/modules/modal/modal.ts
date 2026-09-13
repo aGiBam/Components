d32e8566669fab055b4e507bc369f49b3cb6fc9c
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-13 19:22:45 +0300

    feat(modal): implement Modal component with ElementRef and ngOnInit lifecycle

--- src/app/modules/modal/modal.ts
+++ src/app/modules/modal/modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './modal.css',
   templateUrl: './modal.html',
 })
-export class Modal {}
+export class Modal implements OnInit {
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+   document.body.appendChild(this.el.nativeElement);
+   }
+}
